24a7c89b10f06ecd06fe5ad1c61f06ae4f837d2e
mukuljaitu <mukulgarg334@gmail.com>
2026-04-07 16:50:45 +0530

    Testing Hostinger Final

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Passworddd')" />
+            <x-input-label for="password" :value="__('Passwor')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                 type="password"
